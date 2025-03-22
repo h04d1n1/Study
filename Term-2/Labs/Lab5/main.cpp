3a6ed6caf13cb2cd0272df00092e8d91618837c7
houdini <fedor.z1234@yandex.ru>
2025-03-22 12:25:07 +0300

    Patch Lab5

--- Term-2/Labs/Lab5/main.cpp
+++ Term-2/Labs/Lab5/main.cpp
@@ -1,22 +1,35 @@
-#include"Deposit.cpp"
-#include<iostream>
+#include "Deposit.cpp"
+#include <iostream>
+#include <set>
+#include <unordered_set>
 
 int main()
 {
-    std::unordered_set<Deposit, DepositHasher> st;
+    std::set<Deposit> st;
+    std::unordered_set<Deposit, DepositHasher> unst;
+    
     std::ifstream in("../Term-2/Labs/Lab5/input.txt");
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         std::string lbl, cur;
         int v;
         float r;
         in >> lbl >> v >> cur >> r;
-        std::cout << lbl;
+
         st.insert(Deposit(lbl, v, cur, r));
+        unst.insert(Deposit(lbl, v, cur, r));
     }
     in.close();
 
     std::ofstream out("../Term-2/Labs/Lab5/output.txt");
-    for (auto& pos : st) {
+    for (auto &pos : unst)
+    {
+        out << pos.getLabel() << " " << pos.getValue() << " "
+            << pos.getCurrencyType() << " " << pos.getRate() << "\n";
+    }
+    out << "\n";
+    for (auto &pos : st)
+    {
         out << pos.getLabel() << " " << pos.getValue() << " "
             << pos.getCurrencyType() << " " << pos.getRate() << "\n";
     }
